a6d41c0d1c716ae55793f5f89118af0f793f8990
avasireddi3 <avasireddi3@gmail.com>
2025-05-12 09:41:42 +0530

    Add icons for tech skills

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../co
 
 export default function AboutPage() {
   // Skills data
+
   const skills = {
     technical: [
       "Python",
@@ -89,6 +90,110 @@ export default function AboutPage() {
                       {skill}
                     </div>
                   ))}
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardContent>
+                <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4 justify-items-center pt-6"> 
+                  {/* Java: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/java-original-wordmark.svg" alt="Java" className="h-12 w-12 block dark:hidden" />
+                    <img src="/icons/dark/java-original-wordmark.svg" alt="Java Dark" className="h-12 w-12 hidden dark:block" />
+                  </div>
+
+                  {/* Python: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/python-original-wordmark.svg" alt="Python" className="h-12 w-12 block dark:hidden" />
+                    <img src="/icons/dark/python-original-wordmark.svg" alt="Python Dark" className="h-12 w-12 hidden dark:block" />
+                  </div>
+
+                  {/* Pandas: Font Icon */}
+                  <div className="relative">
+                    <img src="/icons/light/pandas-original-wordmark.svg" alt="Pandas" className="h-12 w-12 block dark:hidden" />
+                    <i className="devicon-pandas-plain-wordmark text-5xl hidden dark:block text-white-500"></i>
+                  </div>
+
+                  {/* Pytest: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/pytest-original-wordmark.svg" alt="Pytest" className="h-12 w-12 block dark:hidden" />
+                    <img src="/icons/dark/pytest-original-wordmark.svg" alt="Pytest Dark" className="h-12 w-12 hidden dark:block" />
+                  </div>
+
+                  {/* Jupyter: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/jupyter-original-wordmark.svg" alt="Jupyter" className="h-12 w-12 block dark:hidden" />
+                    <img src="/icons/dark/jupyter-original-wordmark.svg" alt="Jupyter Dark" className="h-12 w-12 hidden dark:block" />
+                  </div>
+
+                  {/* Airflow: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/apacheairflow-original.svg" alt="Airflow" className="h-9 w-9 block dark:hidden" />
+                    <img src="/icons/dark/apacheairflow-original.svg" alt="Airflow Dark" className="h-9 w-9 hidden dark:block" />
+                  </div>
+
+                  {/* PySpark: Font Icon */}
+                  <div className="relative">
+                  <img src="/icons/light/apachespark-original-wordmark.svg" alt="Spark" className="h-12 w-12 block dark:hidden" />
+                    <i className="devicon-apachespark-plain-wordmark text-5xl hidden dark:block text-red-600"></i>
+                  </div>
+
+                  {/* PostgreSQL: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/postgresql-plain-wordmark.svg" alt="PostgreSQL" className="h-12 w-12 block dark:hidden" />
+                    <img src="/icons/dark/postgresql-plain-wordmark.svg" alt="PostgreSQL Dark" className="h-12 w-12 hidden dark:block" />
+                  </div>
+
+                  {/* Bash Shell: Font Icon */}
+                  <div className="relative">
+                    <i className="devicon-bash-plain text-4xl block dark:hidden"></i>
+                    <i className="devicon-bash-plain text-4xl hidden dark:block text-white-500"></i>
+                  </div>
+
+                  {/* AWS: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/amazonwebservices-plain-wordmark.svg" alt="AWS" className="h-12 w-12 block dark:hidden" />
+                    <img src="/icons/dark/amazonwebservices-plain-wordmark.svg" alt="AWS Dark" className="h-12 w-12 hidden dark:block" />
+                  </div>
+
+                  {/* Google Cloud: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/googlecloud-original.svg" alt="Google Cloud" className="h-12 w-12 block dark:hidden" />
+                    <img src="/icons/dark/googlecloud-original.svg" alt="Google Cloud Dark" className="h-12 w-12 hidden dark:block" />
+                  </div>
+
+                  {/* Git: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/git-plain-wordmark.svg" alt="Git" className="h-12 w-12 block dark:hidden" />
+                    <img src="/icons/dark/git-plain-wordmark.svg" alt="Git Dark" className="h-12 w-12 hidden dark:block" />
+                  </div>
+
+                  {/* Linux: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/linux-original.svg" alt="Linux" className="h-12 w-12 block dark:hidden" />
+                    <img src="/icons/dark/linux-original.svg" alt="Linux Dark" className="h-12 w-12 hidden dark:block" />
+                  </div>
+
+                  
+
+                  {/* React: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/react-original-wordmark.svg" alt="React" className="h-12 w-12 block dark:hidden" />
+                    <img src="/icons/dark/react-original-wordmark.svg" alt="React Dark" className="h-12 w-12 hidden dark:block" />
+                  </div>
+
+                  {/* Javascript: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/javascript-original.svg" alt="Javascript" className="h-11 w-11 block dark:hidden" />
+                    <img src="/icons/dark/javascript-original.svg" alt="Javascript Dark" className="h-11 w-11 hidden dark:block" />
+                  </div>
+
+                  {/* HTML5: Image */}
+                  <div className="relative">
+                    <img src="/icons/light/html5-original-wordmark.svg" alt="Javascript" className="h-11 w-11 block dark:hidden" />
+                    <img src="/icons/dark/html5-plain-wordmark.svg" alt="Javascript Dark" className="h-11 w-11 hidden dark:block" />
+                  </div>
+
                 </div>
               </CardContent>
             </Card>
